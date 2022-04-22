b2e7741863be25bbd35db86173f00dac853aa0e3
Carlos Gerardo <carlosgerardo.rocha.s@gmail.com>
2022-04-21 20:04:36 -0500

    I created a new promise for get  a response that will be use for another function

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,33 +1,44 @@
 require('colors')
 
 
+
 const mostrarMenu = ()=>{
+
+    //a cotninuación cree una promesa puesto que requiero que mi función retorne un valor de respuesta que debe ser retornado para ser recibido por otra función
+
+    return new Promise( resolve()=>{
+
+        console.clear()
     
-    console.clear()
-
-    console.log('======================'.green);
-    console.log(' selecione una opción '.green);
-    console.log('======================\n'.green);
-
-    console.log(`${'1.'.green} Crear nueva tarea`)
-    console.log(`${'2.'.green} Listar tareas`)
-    console.log(`${'3.'.green} Listar tareas completadas`)
-    console.log(`${'4.'.green} Listar tareas pendientes`)
-    console.log(`${'5.'.green} Completar tareas`)
-    console.log(`${'6.'.green} Borrar una tarea`)
-    console.log(`${'0.'.green} Salir \n`)
+        console.log('======================'.green);
+        console.log(' selecione una opción '.green);
+        console.log('======================\n'.green);
     
-    const readline = require('readline').createInterface({
+        console.log(`${'1.'.green} Crear nueva tarea`)
+        console.log(`${'2.'.green} Listar tareas`)
+        console.log(`${'3.'.green} Listar tareas completadas`)
+        console.log(`${'4.'.green} Listar tareas pendientes`)
+        console.log(`${'5.'.green} Completar tareas`)
+        console.log(`${'6.'.green} Borrar una tarea`)
+        console.log(`${'0.'.green} Salir \n`)
+        
+        const readline = require('readline').createInterface({
+    
+            input: process.stdin,
+            output: process.stdout
+    
+        });
+    
+        readline.question('Seleccione una opción:', (opt)=>{
+            readline.close();
+            resolve(opt)
 
-        input: process.stdin,
-        output: process.stdout
+            // el resolve de la promesa va aquí porque el input del usuario de la lista de opciones es el valor que debera ser retirnado como respuesta
+        })
 
-    });
 
-    readline.question('Seleccione una opción:', (opt)=>{
-
-        readline.close();
-    })
+    } )
+    
 
 }
 
